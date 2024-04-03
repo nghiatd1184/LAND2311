0adc5c9ef01a0554493188bca5e7d7af7b8aa09a
nghiatd1184 <nghiatd.dhtm@gmail.com>
2024-04-03 15:10:15 +0700

    15:10

--- app/src/main/java/com/nghiatd/requestpermission/MainActivity.kt
+++ app/src/main/java/com/nghiatd/requestpermission/MainActivity.kt
@@ -3,12 +3,15 @@ package com.nghiatd.requestpermission
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.nghiatd.requestpermission.databinding.ActivityMainBinding
 
@@ -62,23 +65,42 @@ class MainActivity : AppCompatActivity() {
             }
 
             btnOpenGallery.setOnClickListener {
-
+                if (isGranted(permissions)) {
+                    openGallery()
+                } else {
+                    requestCameraMultiPermissionResult.launch(permissions.toTypedArray())
+                }
             }
         }
     }
+
     @Suppress("DEPRECATION")
     private fun openCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         startActivityForResult(intent, 1001)
     }
 
+    @Suppress("DEPRECATION")
+    private fun openGallery() {
+        val i = Intent()
+        i.type = "image/*"
+        i.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(i, 1002)
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         super.onActivityResult(requestCode, resultCode, intent)
-        if (requestCode==1001 && resultCode == RESULT_OK) {
+        if (requestCode == 1001 && resultCode == RESULT_OK) {
             val uri = intent?.data
-            Toast.makeText(this@MainActivity,"$uri",Toast.LENGTH_SHORT).show()
-            mBinding.img.setImageURI(intent?.data)
+            Toast.makeText(this@MainActivity, "$uri", Toast.LENGTH_SHORT).show()
+            mBinding.img.setImageURI(uri)
+        }
+
+        if (requestCode == 1002 && resultCode == RESULT_OK) {
+            val uri = intent?.data
+            Toast.makeText(this@MainActivity, "$uri", Toast.LENGTH_SHORT).show()
+            mBinding.img.setImageURI(uri)
         }
     }
 
